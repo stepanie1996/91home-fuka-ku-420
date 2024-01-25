6457c16d9b07dd080c031c33d336ac0c260e5d8e
stepanie1996 <130530497+stepanie1996@users.noreply.github.com>
2024-01-25 10:05:03 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '91home', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '91狼友之家', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '其实你想看的网上都有', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.91home.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '成人, 暗网, 萝莉, 呦呦, 91, 福利姬, 老王, 缅北, 炼铜, 门事件, 资源', // 网站关键词 英文逗号隔开
